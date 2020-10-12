152af87a8af0643aba71e67033e6c8374d210606
Lukasz Modzelewski <lukaszmod@o2.pl>
2020-10-12 19:36:08 +0200

    feat: messages, styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './styles.css';
+import Message from './components/Message/index'
 
-function App() {
+export default function App() {
+
+  const messages = [
+    { id: 12, user: 'Patryk', content: 'Hej co tam', datetime: Date.now() },
+    { id: 24, user: 'Ania', content: 'Hejjoooo', datetime: Date.now() },
+    { id: 67, user: 'Patryk', content: 'Lubblin tutaj', datetime: Date.now() }];
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {messages.map(message => (
+        <div><Message key={message.id} message={message} /></div>
+      ))}
     </div>
   );
 }
-
-export default App;
