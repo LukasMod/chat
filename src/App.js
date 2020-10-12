18692277431d3690ab97bdc6207fc2c148d79998
Lukasz Modzelewski <lukaszmod@o2.pl>
2020-10-12 21:02:58 +0200

    feat: connect with realtime database

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
 import Message from './components/Message/Message'
 import MessageForm from './components/MessageForm/MessageForm'
@@ -7,32 +7,47 @@ import db from './config'
 
 export default function App() {
 
-  const [message, setMessage] = useState('');
+  const [newMessage, setNewMessage] = useState('');
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    db.ref('/messages').on('value', (snapshot) => {
+      const fbMessages = snapshot.val();
+      const convertedMessages = Object.entries(fbMessages || {}).map(([id, message]) => ({ ...message, id }));
+      setMessages(convertedMessages)
+      console.log(convertedMessages)
+
+    })
+  }, [])
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const messageObj = {
       user: 'Patryk',
-      content: message,
+      content: newMessage,
       datetime: Date.now()
     }
+    if (newMessage) {
+      db.ref('/messages').push(messageObj)
+    }
     console.log("Message: ", messageObj)
-    db.ref('/messages').push(messageObj)
-    setMessage('')
+
+    setNewMessage('')
   }
 
 
-  const messages = [
-    { id: 11, user: 'Patryk', content: 'Hej co tam', datetime: Date.now() },
-    { id: 22, user: 'Ania', content: 'Hejjoooo', datetime: Date.now() },
-    { id: 33, user: 'Patryk', content: 'Lubblin tutaj', datetime: Date.now() }];
+  // const messages = [
+  //   { id: 11, user: 'Patryk', content: 'Hej co tam', datetime: Date.now() },
+  //   { id: 22, user: 'Ania', content: 'Hejjoooo', datetime: Date.now() },
+  //   { id: 33, user: 'Patryk', content: 'Lubblin tutaj', datetime: Date.now() }];
 
   return (
     <div className="App">
       {messages.map(message => (
         <Message key={message.id} message={message} />
       ))}
-      <MessageForm content={message.content} handleSubmit={handleSubmit}
-        handleContentChange={setMessage} />
+      <MessageForm content={newMessage.content} handleSubmit={handleSubmit}
+        handleContentChange={setNewMessage} />
     </div>
   );
 }
